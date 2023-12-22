7ea1426f260ee20d96205c380951fa6207e51608
simon <simon.bela.bumiller@gmx.de>
2023-12-22 12:23:40 +0100

    save timestampf for creating and updating

--- src/main/kotlin/com/snow/aboutme/data/model/base/AbstractEntity.kt
+++ src/main/kotlin/com/snow/aboutme/data/model/base/AbstractEntity.kt
@@ -4,12 +4,22 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.CreationTimestamp
+import org.hibernate.annotations.SourceType
+import org.hibernate.annotations.UpdateTimestamp
+import java.time.Instant
 
 @MappedSuperclass
 abstract class AbstractEntity (
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null
+    val id: Long? = null,
+
+    @CreationTimestamp(source = SourceType.DB)
+    val created: Instant? = null,
+
+    @UpdateTimestamp(source = SourceType.DB)
+    val updated: Instant? = null
 
 )
